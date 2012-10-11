eeea0a2d31a25db767690730432cbe6464296e4c
doubek <mr.jan.doubek@gmail.com>
2012-10-11 09:14:17 +0200

    Struktura java projektu

---
+++ kosapi-java/src/main/java/cz/fjfi/pvs/kosapi/App.java
@@ -0,0 +1,13 @@
+package cz.fjfi.pvs.kosapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
