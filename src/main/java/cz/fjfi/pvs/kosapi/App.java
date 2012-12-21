62cb560bcd72e94da92d895f35413958ae2ebfe1
kajinond <kajinond@fjfi.cvut.cz>
2012-12-21 14:21:51 +0100

    Done some code tiding

--- src/main/java/cz/fjfi/pvs/kosapi/App.java
+++ src/main/java/cz/fjfi/pvs/kosapi/App.java
@@ -5,19 +5,12 @@ import cz.fjfi.pvs.kosapi.statistic.ExamVsNonexamStatistic;
 import cz.fjfi.pvs.kosapi.web.KosAtomReader;
 import java.io.IOException;
 import java.util.Hashtable;
-import java.util.Properties;
 
 
 public class App 
 {
     public static void main( String[] args ) throws IOException
-    {
-        /*
-        KosAtomReader kosReader = new KosAtomReader("divisions");
-        String response = kosReader.getKosResponse();
-        System.out.println(response);
-        */
-        
+    {        
     	KosAtomReader coursesReader = new KosAtomReader("courses");
     	String coursesResponse = coursesReader.getKosResponse();
     	ExamVsNonexamStatistic examsVsNonexams = new ExamVsNonexamStatistic(coursesResponse);
@@ -25,13 +18,6 @@ public class App
     	
     	Hashtable<String, Double> statisticValues = examsVsNonexams.getStatisticValues();
     	PieChart3D pieChart = new PieChart3D(statisticValues, "Courses chart");
-    	pieChart.saveAsPNG("/tmp/chart");
-    }
-    
-    public static Properties loadProperties(String properties) throws IOException{
-        String fullPropertiesName = properties + ".properties";
-        Properties settings = new Properties();
-        settings.load(App.class.getResourceAsStream(fullPropertiesName));
-        return settings;
+    	pieChart.saveAsPNG("tmp/chart");
     }
 }
