5e969a794363916b231bde1614b9d4e445fdf9ae
kajinond <kajinond@fjfi.cvut.cz>
2013-01-03 14:57:27 +0100

    changed app definition to use input arguments for describing its behaviour

--- src/main/java/cz/fjfi/pvs/kosapi/App.java
+++ src/main/java/cz/fjfi/pvs/kosapi/App.java
@@ -1,7 +1,7 @@
 package cz.fjfi.pvs.kosapi;
 
 import cz.fjfi.pvs.kosapi.chart.PieChart3D;
-import cz.fjfi.pvs.kosapi.statistic.ExamVsNonexamStatistic;
+import cz.fjfi.pvs.kosapi.statistic.ExamVsCreditStatistic;
 import cz.fjfi.pvs.kosapi.statistic.TeachersPerDivisionStatistic;
 import cz.fjfi.pvs.kosapi.web.KosAtomReader;
 import java.net.URL;
@@ -15,37 +15,80 @@ public class App
 {
     static Logger logger = Logger.getLogger(App.class);
     
+    enum expectedArgs { courses, teachers, help };
+    
     public static void main( String[] args )
     {   
         loadLoggerProperties();
         logger.info("Starting application");
-        try 
+        if(args.length > 0)
         {
-            KosAtomReader coursesReader = new KosAtomReader("courses");
-            String coursesResponse = coursesReader.getKosResponse();
-            ExamVsNonexamStatistic examsVsNonexams = new ExamVsNonexamStatistic(coursesResponse, "completion");
-            examsVsNonexams.printStatisticValues();
+        	for(String inputArg: args)
+        	{
+        		expectedArgs arg = expectedArgs.valueOf(inputArg);
+        		switch(arg)
+        		{
+        		case courses:
+        			try 
+        	        {
+        	            KosAtomReader coursesReader = new KosAtomReader("courses");
+        	            String coursesResponse = coursesReader.getKosResponse();
+        	            ExamVsCreditStatistic examsVsNonexams = new ExamVsCreditStatistic(coursesResponse, "completion");
+        	            examsVsNonexams.printStatisticValues();
 
-            Map<String, Double> examsStatisticValues = examsVsNonexams.getStatisticValues();
-            PieChart3D examsChart = new PieChart3D(examsStatisticValues, "Courses chart");
-            examsChart.saveAsPNG("tmp/examsVsNonexams");
-            
-            KosAtomReader teachersReader = new KosAtomReader("teachers");
-            String teachersResponse = teachersReader.getKosResponse();
-            TeachersPerDivisionStatistic teachersPerDivision = new TeachersPerDivisionStatistic(teachersResponse, "division");
-            teachersPerDivision.printStatisticValues();
-            
-            Map<String, Double> teachersStatisticValues = teachersPerDivision.getStatisticValues();
-            PieChart3D teachersChart = new PieChart3D(teachersStatisticValues, "Teachers Per Division chart");
-            teachersChart.saveAsPNG("tmp/teachersPerDivision");
-        } 
-        catch (Exception e) 
+        	            Map<String, Double> examsStatisticValues = examsVsNonexams.getStatisticValues();
+        	            PieChart3D examsChart = new PieChart3D(examsStatisticValues, "Courses chart");
+        	            examsChart.saveAsPNG("tmp/examsVsNonexams");
+        	        }
+        			catch (Exception e) 
+        	        {
+        	            logger.error(e.getMessage(), e);
+        	        }
+        			break;
+        		case teachers:
+        			try 
+        	        {
+        	            KosAtomReader teachersReader = new KosAtomReader("teachers");
+        	            String teachersResponse = teachersReader.getKosResponse();
+        	            TeachersPerDivisionStatistic teachersPerDivision = new TeachersPerDivisionStatistic(teachersResponse, "division");
+        	            teachersPerDivision.printStatisticValues();
+        	            
+        	            Map<String, Double> teachersStatisticValues = teachersPerDivision.getStatisticValues();
+        	            PieChart3D teachersChart = new PieChart3D(teachersStatisticValues, "Teachers Per Division chart");
+        	            teachersChart.saveAsPNG("tmp/teachersPerDivision");
+        	        } 
+        	        catch (Exception e) 
+        	        {
+        	            logger.error(e.getMessage(), e);
+        	        }
+        			break;
+        		case help:
+        			help("Help");
+        			break;
+        		default:
+        			help("Wrong argument");
+        			break;
+        		}
+        	}
+        }
+        else
         {
-            logger.error(e.getMessage(), e);
+        	help("No input arguments");
         }
     }
     
-    public static void loadLoggerProperties () {
+    private static void help(String errorMessage)
+    {
+    	loadLoggerProperties();
+    	logger.info(errorMessage);
+    	logger.info("Usage: java App.java <args>");
+    	logger.info("Possible args:");
+    	logger.info("\tcourses: prints statistic of courses completed by exams or by credit");
+    	logger.info("\tteachers: prints number of teacher per division");
+    	logger.info("\thelp: prints this help");
+    }
+    
+    private static void loadLoggerProperties () {
         URL log4Jresource = App.class.getResource("logger.properties");
         PropertyConfigurator.configure(log4Jresource);
     }
---
+++ src/main/java/cz/fjfi/pvs/kosapi/statistic/ExamVsCreditStatistic.java
@@ -0,0 +1,40 @@
+package cz.fjfi.pvs.kosapi.statistic;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import cz.fjfi.pvs.kosapi.parser.AtomTagParser;
+
+public class ExamVsCreditStatistic extends Statistics{
+	
+	private final String credit = "CREDIT";
+	private final String creditExam = "CREDIT_EXAM";
+
+	public ExamVsCreditStatistic(String kosResponse, String requiredTag)
+	{
+		super(kosResponse, requiredTag);
+	}
+
+	@Override
+	protected Map<String, Double> computeStatistic(String requiredTag)
+	{		
+		List<AtomTagParser> completionsList = getKosTagsByName(requiredTag);
+		Map<String, Integer> completionsCount = new Hashtable<String, Integer>();
+		completionsCount.put(credit, 0);
+		completionsCount.put(creditExam, 0);
+		Iterator<AtomTagParser> completionsIterator = completionsList.iterator();
+		while(completionsIterator.hasNext())
+		{
+			AtomTagParser completionTag = completionsIterator.next();
+			String completion = completionTag.getTagContent();
+			completionsCount.put(completion, completionsCount.get(completion) + 1);
+		}
+		Map<String, Double> results = new Hashtable<String, Double>();
+		results.put(credit, (double)completionsCount.get(credit));
+		results.put(creditExam, (double)completionsCount.get(creditExam));
+		return results;
+	}
+
+}
